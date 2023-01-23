45c8538472e45eaa86b40b62c14dd0df877ab9e2
Kristjanmn <kristjanmn@hotmail.com>
2023-01-24 01:04:18 +0200

    Created nodejs server with handlebars.

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import {create} from 'express-handlebars';
+
+import * as fs from 'node:fs';
+import * as path from 'path';
+import {fileURLToPath} from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const PORT = 3000;
+const app = express();
+const hbs = create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('home', {title: "Home"});
+});
+
+app.get('/hbs/*', (req, res) => {
+    res.render(req.params[0]);
+});
+
+app.get('/js/*', (req, res) => {
+    res.send(fs.readFileSync(__dirname+req.path+'.js').toString());
+});
+
+app.listen(PORT, () => {
+    console.log("Nodejs server running on 172.17.0.2:%s", PORT);
+});
