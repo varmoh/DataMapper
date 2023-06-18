2a35c3c08a6f0a731c788729c907a5a15d78ed3a
ahmedyasser <ahmedyasser.dev@hotmail.com>
2023-06-18 03:28:36 +0300

    Added Triple DES Encryption/Decryption

--- controllers/decryption.js
+++ controllers/decryption.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import aesDecrypt from '../js/decryption/aes.js';
+import tripleDesDecrypt from '../js/decryption/triple-des.js';
 
 const router = express.Router();
 
 router.post('/aes', async (req, res) => {
   const result = await aesDecrypt(req.body.cipher, req.body.key, req.body.isObject)
+  return res.status(result.error ? 400 : 200).json(result)
+})
+
+router.post('/triple-des', async (req, res) => {
+  const result = await tripleDesDecrypt(req.body.cipher, req.body.key, req.body.isObject)
   return res.status(result.error ? 400 : 200).json(result)
 })
 
--- controllers/encryption.js
+++ controllers/encryption.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import aesEncrypt from '../js/encryption/aes.js';
+import tripleDesEncrypt from '../js/encryption/triple-des.js';
 
 const router = express.Router();
 
 router.post('/aes', async (req, res) => {
   const result = await aesEncrypt(req.body.content, req.body.key)
+  return res.status(result.error ? 400 : 200).json(result)
+})
+
+router.post('/triple-des', async (req, res) => {
+  const result = await tripleDesEncrypt(req.body.content, req.body.key)
   return res.status(result.error ? 400 : 200).json(result)
 })
 
